9aba182e6ce2220f38562951e04e89219d497796
Never <Never1750@163.com>
2017-09-29 21:33:13 +0800

    Initial Commit

---
+++ ECO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ECO
+//
+//  Created by Never on 2017/9/29.
+//  Copyright © 2017年 Never. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
